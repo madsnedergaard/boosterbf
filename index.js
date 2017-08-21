eb69793d9b79070f644e43e98c62fb526af3daf3
Mads Nedergaard <nedergaardmads@gmail.com>
2017-08-21 23:30:34 +0200

    Booster BF MVP done

--- index.js
+++ index.js
@@ -3,8 +3,53 @@ const request = require('request')
 var bodyParser = require('body-parser')
 var slack = require('slack')
 
+require('dotenv').config()
+var token = process.env.SLACK_TOKEN
+
 const app = express()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var message = {
+  text: "Nogen der er klar på en kamp, <!here>?",
+  attachments: [
+  {
+    fields: [
+    {
+      "title": "Spillere",
+      value: "1.\n2.\n3.\n4.\n",
+      "short": true
+    }
+    // {
+    //   "title": "Substitues (later maybe)",
+    //   "value": "1. \n2.\n3.\n4.",
+    //   "short": true
+    // }
+
+    ]
+  },
+  {
+    "fallback": "Er du klar på et spil?",
+    "title": "Er du klar på et spil?",
+    "callback_id": "comic_1234_xyz",
+    "color": "#000",
+    "attachment_type": "default",
+    "actions": [
+    {
+      "name": "join",
+      "text": "Så klar!",
+      "type": "button",
+      "style": "primary",
+      "value": "join"
+    }
+    // {
+    //   "name": "later",
+    //   "text": "Later maybe",
+    //   "type": "button",
+    //   "value": "later"
+    // }
+    ]
+  }
+  ]
+}
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -14,141 +59,84 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
+// User requests new match
 app.post('/commands', urlencodedParser, (req, res) => {
-	console.log('commands');
+  console.log('commands');
   res.status(200).end() // best practice to respond with empty 200 status code
   var reqBody = req.body
+
   var responseURL = reqBody.response_url;
-  var message = {
-    "text": "Mads has requested a match, @here!",
-    "attachments": [
-        {
-            "fields": [
-                {
-                    "title": "Players",
-                    "value": "1. Mads\n2.\n3.\n4.",
-                    "short": true
-                },
-                {
-                    "title": "Substitues (later maybe)",
-                    "value": "1. \n2.\n3.\n4.",
-                    "short": true
-                }
-
-            ]
-        },
-        {
-            "fallback": "Are you ready to join the match?",
-            "title": "Are you ready to join the match?",
-            "callback_id": "comic_1234_xyz",
-            "color": "#5AE664",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "join",
-                    "text": "Yes, I'm ready now",
-                    "type": "button",
-                    "style": "primary",
-                    "value": "join"
-                },
-                {
-                    "name": "later",
-                    "text": "Later maybe",
-                    "type": "button",
-                    "value": "later"
-                }
-            ]
-        }
-    ]
-}
-var token = reqBody.token
-var channel = reqBody.channel.id
-slack.chat.postMessage({token, channel, message}, (err, data) => { console.log('yo')})
-
-
-//  sendMessageToSlackResponseURL(responseURL, message)
+    slack.chat.postMessage({
+        token: token, 
+        channel: reqBody.channel_id, 
+        text: message.text,
+        link_names: 1,
+        attachments: message.attachments
+    }
+    , 
+    (err, data) => { 
+      console.log('ERR: --------', err);
+      console.log('DATA: --------', data);
+    })
 })
 
 
 app.post('/actions', urlencodedParser, (req, res) => {
+  console.log('actions');
   res.status(200).end()
   const actionJSONPayload = JSON.parse(req.body.payload) // parse URL-encoded payload JSON string
   console.log(actionJSONPayload);
-  const message = {
-    "text": actionJSONPayload.user.name+" clicked: "+actionJSONPayload.actions[0].name,
-    "replace_original": false
+
+  const newList = addPlayerToList(actionJSONPayload.user.name,message.attachments);
+
+  slack.chat.update({
+    token: token, 
+    channel: actionJSONPayload.channel.id, 
+    text: message.text,
+    attachments: newList,
+    link_names: 1,
+    ts: actionJSONPayload.message_ts
   }
-  //sendMessageToSlackResponseURL(actionJSONPayload.response_url, message)
-  const token = actionJSONPayload.token;
-  const ts = actionJSONPayload.message_ts;
-  const channel = actionJSONPayload.channel.id;
-  const text = "How goes";
-	slack.chat.update({token, ts, channel, text}, (err, data) => {
-		console.log(err);
-		console.log(data);
-	})
-
-})
-/*
-/*
-app.post('/commands/play', urlencodedParser, (req, res) =>{
-    res.status(200).end() // best practice to respond with empty 200 status code
-    var reqBody = req.body
-    var responseURL = reqBody.response_url
-    if (reqBody.token != YOUR_APP_VERIFICATION_TOKEN){
-        res.status(403).end("Access forbidden")
-    }else{
-        var message = {
-            "text": "This is your first interactive message",
-            "attachments": [
-                {
-                    "text": "Building buttons is easy right?",
-                    "fallback": "Shame... buttons aren't supported in this land",
-                    "callback_id": "button_tutorial",
-                    "color": "#3AA3E3",
-                    "attachment_type": "default",
-                    "actions": [
-                        {
-                            "name": "yes",
-                            "text": "yes",
-                            "type": "button",
-                            "value": "yes"
-                        },
-                        {
-                            "name": "no",
-                            "text": "no",
-                            "type": "button",
-                            "value": "no"
-                        },
-                        {
-                            "name": "maybe",
-                            "text": "maybe",
-                            "type": "button",
-                            "value": "maybe",
-                            "style": "danger"
-                        }
-                    ]
-                }
-            ]
-        }
-        sendMessageToSlackResponseURL(responseURL, message)
-    }
+  , 
+  (err, data) => { 
+    console.log('ERR: --------', err);
+    console.log('DATA: --------', data);
+  })
 })
 
 
-*/
-function sendMessageToSlackResponseURL(responseURL, JSONmessage){
-    var postOptions = {
-        uri: responseURL,
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        json: JSONmessage
-    }
-    request(postOptions, (error, response, body) => {
-        if (error){
-            // handle errors as you see fit
-        }
-    })
+function addPlayerToList(player, list) {
+  // Check if self exists in list
+  var field = list[0].fields[0].value;
+  if (field.indexOf(player) !== -1) {
+    return list;
+  }
+  if (field.indexOf('1.\n') !== -1) {
+    list[0].fields[0].value = field.replace('1.', '1. ' + player);
+  } else if (field.indexOf('2.\n') !== -1) {
+    list[0].fields[0].value = field.replace('2.', '2. ' + player);
+  } else if (field.indexOf('3.\n') !== -1) {
+    list[0].fields[0].value = field.replace('3.\n', '3. ' + player + '\n');
+  } else if (field.indexOf('4.\n') !== -1) {
+    list[0].fields[0].value = field.replace('4.\n', '4. ' + player + '\n');
+    list = gameReadyMessage(list);
+  } else {
+    // List is full
+  }
+  return list;
+}
+
+function gameReadyMessage(list) {
+  var field = list[0].fields[0].value;
+  var names = field.match(/([A-Z])\w+/gi);
+  for (var i = 0; i < names.length; i++) {
+    names[i] = '<@' + names[i] + '>';
+  }
+  var nameString = names.join(', ');
+  list[1].fallback = 'All set, let\'s go! \n('+nameString+')';
+  list[1].title = 'All set, let\'s go! \n('+nameString+')';
+  list[1].color = '#5AE664';
+  list[1].actions = [];
+
+  return list;
 }
